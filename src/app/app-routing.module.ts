a1cdc0d40b7857c1a50880d8cf6d1c72e6d252f9
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2019-04-27 22:10:36 +0530

    ADD: routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  {
+    path: 'audio-detail', loadChildren: './audio-detail/audio-detail.module#AudioDetailPageModule'
+  },
+  { path: 'darshan-gallery', loadChildren: './darshan-gallery/darshan-gallery.module#DarshanGalleryPageModule' },
+  { path: 'video-detail', loadChildren: './video-detail/video-detail.module#VideoDetailPageModule' }
 ];
 @NgModule({
   imports: [
