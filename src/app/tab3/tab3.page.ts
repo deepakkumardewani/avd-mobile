de18c478a092226d8f339c294a256933e53581c4
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2019-05-05 20:21:17 +0530

    ADD: video duration

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -53,4 +53,19 @@ export class Tab3Page {
     });
   }
 
+  YTDurationToSeconds(duration) {
+    let match = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/);
+    match = match.slice(1).map(function (x) {
+      if (x != null) {
+        return x.replace(/\D/, '');
+      }
+    });
+
+    const hours = (parseInt(match[0]) || 0);
+    const minutes = (parseInt(match[1]) || 0);
+    const seconds = (parseInt(match[2]) || 0);
+
+    return hours * 3600 + minutes * 60 + seconds;
+  }
+
 }
