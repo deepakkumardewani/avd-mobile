dc5fda2cdf4163efcd451ef2a08580ceac45454c
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2019-05-19 12:28:34 +0530

    ADD: date ago pipe

--- src/app/pipe.module.ts
+++ src/app/pipe.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { ShortNumberPipe } from './pipes/short-number.pipe';
 import { DurationPipe } from './pipes/duration.pipe';
+import { DateAgoPipe } from './pipes/date-ago.pipe';
 @NgModule({
   imports: [],
   declarations: [
     ShortNumberPipe,
-    DurationPipe
+    DurationPipe,
+    DateAgoPipe
   ],
   exports: [
     ShortNumberPipe,
-    DurationPipe
+    DurationPipe,
+    DateAgoPipe
   ]
 })
 export class PipeModule {}
